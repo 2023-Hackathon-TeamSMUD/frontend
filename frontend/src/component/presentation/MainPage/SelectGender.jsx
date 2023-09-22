a7ddb38a2d354eea46975b5b50a0a9adc59556bd
zzangjyj0818 <jyj010818@naver.com>
2023-09-22 21:18:45 +0900

    Implement the GenderSelectPage

--- frontend/src/component/presentation/MainPage/SelectGender.jsx
+++ frontend/src/component/presentation/MainPage/SelectGender.jsx
@@ -1,8 +1,14 @@
-import React from 'react';
+import React, {useState} from 'react';
 import '../../../styles/components/MainPage/SelectGender.scss';
 import Progress2 from '../../../assets/MainPage/Progress2.png'
+import Man from '../../../assets/MainPage/Man.png'
+import Woman from '../../../assets/MainPage/Woman.png'
+import ActiveMan from '../../../assets/MainPage/ActiveMan.png'
+import ActiveWoman from '../../../assets/MainPage/ActiveWoman.png'
 
 const SelectGender = () => {
+    const [gender, setGender] = useState();
+
     return (
         <div className='SelectGender_Container'>
             <div className='SelectGender_Wrapper'>
@@ -12,12 +18,8 @@ const SelectGender = () => {
                     <div className='SelectGender_TextBox2'>성별에 맞춰 코디를 알려드립니다!</div>
                 </div>
                 <div className='SelectGender_SelectLayOut'>
-                    <div>
-
-                    </div>
-                    <div>
-
-                    </div>
+                    {gender === '남성' ? <img src={ActiveMan} alt='' onClick={()=>setGender()}/> : <img src={Man} alt='' onClick={()=>setGender('남성')}/>}
+                    {gender === '여성' ? <img src={ActiveWoman} alt='' onClick={()=>setGender()}/> : <img src={Woman} alt='' onClick={()=>setGender('여성')}/>}
                 </div>
                 <button className='SelectGender_Btn'>다음</button>
             </div>
